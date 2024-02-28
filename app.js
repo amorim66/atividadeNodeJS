0520115ff9ee87652551606d74e489f1c53baa21
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2024-02-28 11:06:16 -0300

    correção

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const port = 8081;
 
 app.listen(port, () => {
     console.log(`Servidor está ouvindo na porta ${port}`);
-  });
+});
 
-  app.get('/produtos/automovel/:marca/:modelo/:ano', (req, res) => {
+app.get('/produtos/automovel/:marca/:modelo/:ano', (req, res) => {
     const { marca, modelo, ano } = req.params;
     res.send(`<h1>&nbsp;Carros</h1>&nbsp;&nbsp;Detalhes do automóvel:<br><br>&nbsp;&nbsp;Marca - ${marca}<br>&nbsp;&nbsp;Modelo - ${modelo}<br>&nbsp;&nbsp;Ano - ${ano}`);
-  });
+});
 
-  app.get('/cadastrar/usuario', (req,res) => {
+app.get('/cadastrar/usuario', (req,res) => {
     res.send(`<h1>Página de cadastro de usuário</h1>`)
-  });
+});
   
-  app.get('/contato', (req,res) => {
+app.get('/contato', (req,res) => {
     res.send(`<h1>Contato</h1>`)
-  });
+});
